378d406481f0c967e0db14e43a61e6c1a89a0b26
Roman_Peter <roman.p940@gmail.com>
2020-03-15 07:59:45 +0100

    Overlay

---
+++ Logic.java
@@ -0,0 +1,18 @@
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+
+public class Logic {
+    public static void main(String[] args) {
+        try {
+            GlobalScreen.registerNativeHook();
+        }
+        catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+
+            System.exit(1);
+        }
+
+        GlobalScreen.addNativeKeyListener(new KeyListener());
+    }
+}
---
+++ Overlay.java
@@ -0,0 +1,41 @@
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class Overlay extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+
+    public Overlay() {
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Double width = screenSize.getWidth();
+        Double height = screenSize.getHeight();
+
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setUndecorated(true);
+        this.setBackground(new Color(0, 0, 0, 0));
+        this.setSize(width.intValue(), height.intValue());
+        this.setPreferredSize(new Dimension(width.intValue(), height.intValue()));
+        this.getRootPane().putClientProperty("apple.awt.draggableWindowBackground", false);
+        this.setLayout(null);
+        this.setAlwaysOnTop(true);
+        this.setVisible(true);
+    }
+
+    public void paint(Graphics g)
+    {
+        super.paint(g);
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+        g.drawString("Handcards:", 1300, 1070);
+        g.drawString("Friendly Minions: ", 440, 540);
+        g.drawString("Enemy Minions: ", 440, 480);
+        g.drawString("E", 1660, 500);
+        g.fillRect(100,  200,20, 50);
+    }
+
+    public static void main(String[] args) {
+        Overlay overlay = new Overlay();
+    }
+}
